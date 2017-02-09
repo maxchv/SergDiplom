52d9ea5368c43ed52430b809d8b39af7e348a919
Maxim <maxshaptala@gmail.com>
2017-02-09 16:28:09 +0200

    Добавлена форма регистрации на главную страницу

--- static/landing/js/script.js
+++ static/landing/js/script.js
@@ -3,7 +3,7 @@
  */
 $(function () {
     $("#enter").click(function () {
-        // alert("hello");
+        // форма входа
         $("#login").show();
         $('#background').fadeIn(1200);
         $("#login").animate({top: "200px"}, 1200, function () {
@@ -11,10 +11,25 @@ $(function () {
         });
     });
 
+    function login_up() {
+        $("#login:visible").animate({top: "-300px"}, 1200, function () {
+            $("#login").hide();
+        });
+    }
+
+    // форма регистрации
+    $("#btn-register").click(function () {
+        login_up();
+         $("#register").delay(1000).animate({top: "200px"}, 1200, function () {
+            $("#id_username").focus();
+        });
+    });
+
     $("#background").click(function () {
         $(this).fadeOut(1200);
-        $("#login").animate({top: "-300px"}, 1200, function () {
-            $("#login").hide();
+        login_up();
+        $("#register:visible").animate({top: "-300px"}, 1200, function () {
+            $("#register").hide();
         });
     });
 });
