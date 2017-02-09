dbf617b93f4b2195778d26b80a2a3f685aaceb97
Maxim <maxshaptala@gmail.com>
2017-02-09 12:46:09 +0200

    Обновлена модель для фильтров по лакам

--- gallery/models.py
+++ gallery/models.py
@@ -3,7 +3,9 @@ from django.db import models
 
 class Photo(models.Model):
     image = models.ImageField(verbose_name="Изображение", upload_to="photos")
-    description = models.CharField(verbose_name="Краткое описание", max_length=50)
+    title = models.CharField(blank=True, verbose_name="Заголовок", max_length=50)
+    description = models.CharField(verbose_name="Краткое описание", max_length=150)
+    is_nail_polish = models.BooleanField(verbose_name='Лаки', default=False)
 
     def __str__(self):
         return self.description
--- xXx/settings.py
+++ xXx/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+yvc$wwoc#twzb#rg(e#o@x=id-@(7lv+q(a@423l*x_yf4ilg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['the-beauty-salon.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
